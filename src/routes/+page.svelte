a13253b067ac246e4b0f7997dd0d9e1858c9133b
Ammie Xie <atlus0927@Ammies-Air.lan>
2024-05-07 17:14:19 -0700

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,7 @@
   import App from '../components/App.svelte';
 </script>
 
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
 <App />
